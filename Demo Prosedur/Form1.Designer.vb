1f09a4d647b414bb0c3053afe4b24932cc935569
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2020-07-29 09:54:57 +0700

    Add project files.

---
+++ Demo Prosedur/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.listKeluaran = New System.Windows.Forms.ListBox()
+        Me.tombolJalankan = New System.Windows.Forms.Button()
+        Me.tombolKeluar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'listKeluaran
+        '
+        Me.listKeluaran.FormattingEnabled = True
+        Me.listKeluaran.Location = New System.Drawing.Point(13, 13)
+        Me.listKeluaran.Name = "listKeluaran"
+        Me.listKeluaran.Size = New System.Drawing.Size(303, 199)
+        Me.listKeluaran.TabIndex = 0
+        '
+        'tombolJalankan
+        '
+        Me.tombolJalankan.Location = New System.Drawing.Point(13, 219)
+        Me.tombolJalankan.Name = "tombolJalankan"
+        Me.tombolJalankan.Size = New System.Drawing.Size(100, 31)
+        Me.tombolJalankan.TabIndex = 1
+        Me.tombolJalankan.Text = "Jalankan!"
+        Me.tombolJalankan.UseVisualStyleBackColor = True
+        '
+        'tombolKeluar
+        '
+        Me.tombolKeluar.Location = New System.Drawing.Point(204, 218)
+        Me.tombolKeluar.Name = "tombolKeluar"
+        Me.tombolKeluar.Size = New System.Drawing.Size(112, 31)
+        Me.tombolKeluar.TabIndex = 2
+        Me.tombolKeluar.Text = "Keluar"
+        Me.tombolKeluar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(328, 262)
+        Me.Controls.Add(Me.tombolKeluar)
+        Me.Controls.Add(Me.tombolJalankan)
+        Me.Controls.Add(Me.listKeluaran)
+        Me.Name = "Form1"
+        Me.Text = "Demo Prosedur"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents listKeluaran As System.Windows.Forms.ListBox
+    Friend WithEvents tombolJalankan As System.Windows.Forms.Button
+    Friend WithEvents tombolKeluar As System.Windows.Forms.Button
+
+End Class
---
+++ Demo Prosedur/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Sub TampilPesan()
+        'Ini adalah sebuah prosedur yang menampilkan sebuah pesan.
+        listKeluaran.Items.Add("")
+        listKeluaran.Items.Add("Hallo dari prosedur TampilPesan.")
+        listKeluaran.Items.Add("")
+    End Sub
+
+    Private Sub tombolJalankan_Click(sender As Object, e As EventArgs) Handles tombolJalankan.Click
+        'Menampilkan teks tertentu di dalam kotak list.
+        listKeluaran.Items.Add("Hallo dari prosedur tombolJalankan_Click.")
+        listKeluaran.Items.Add("Sekarang, Saya memanggil prosedur TampilPesan.")
+
+        'Memanggil prosedur TampilPesan
+        TampilPesan()
+
+        'Menampilkan teks tertentu pada kotak list
+        listKeluaran.Items.Add("Sekarang, Saya kembali ke prosedur tombolJalankan_Click.")
+    End Sub
+
+    Private Sub tombolKeluar_Click(sender As Object, e As EventArgs) Handles tombolKeluar.Click
+        Application.Exit()
+    End Sub
+End Class
